aa4c449febd2f0c92e964feade6c179be1ce7e67
Gregory Choice <c9311718@uon.edu.au>
2019-06-04 19:41:28 +1000

    [Fix] Move emptying of car to PaymentConfirmation Page_Load

--- Assignment_2/UL/Payment.aspx.cs
+++ Assignment_2/UL/Payment.aspx.cs
@@ -62,8 +62,6 @@ namespace Assignment_2.UL
 
 						int rows = BLPurchase.storePurchase(purchase, card);
 
-						Session.Remove("Cart");
-						Session["Cart"] = new BLShoppingCart();
 						Response.Redirect("~/UL/PaymentConfirmation.aspx");
 						break;
 					case TransactionResult.Declined:
