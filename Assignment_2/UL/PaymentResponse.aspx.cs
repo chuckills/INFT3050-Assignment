8f9e9af4cb29d6d19fe70865118f21adf141749e
Gregory Choice <c9311718@uon.edu.au>
2019-06-07 11:03:24 +1000

    [Fix] Secure PaymentResponse page

--- Assignment_2/UL/PaymentResponse.aspx.cs
+++ Assignment_2/UL/PaymentResponse.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -10,15 +11,25 @@ namespace Assignment_2.UL
     public partial class PaymentResponse : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-            // Page only accessible by logged in user
-            if (Session["LoginStatus"].Equals("User"))
+		{
+			// Check for secure connection
+			if (Request.IsSecureConnection)
+			{
+				// Page only accessible by logged in user
+				if (Session["LoginStatus"].Equals("User"))
+				{
+					lblResult.Text = Session["Result"].ToString();
+				}
+				else
+				{
+					Response.Redirect("~/UL/ErrorPage/0");
+				}
+			}
+			else
             {
-                lblResult.Text = Session["Result"].ToString();
-            }
-            else
-            {
-                Response.Redirect("~/UL/ErrorPage/0");
+                // Make connection secure if it isn't already
+                string url = ConfigurationManager.AppSettings["SecurePath"] + "PaymentConfirmation";
+                Response.Redirect(url);
             }
         }
     }
